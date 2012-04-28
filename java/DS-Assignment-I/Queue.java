ff917bfd13e196881f0b243be143cc530fed9113
orelma <orelma@post.bgu.ac.il>
2012-04-28 18:24:02 +0300

    Update java/DS-Assignment-I/Queue.java

--- java/DS-Assignment-I/Queue.java
+++ java/DS-Assignment-I/Queue.java
@@ -1,12 +1,9 @@
 /**
- * 
- */
-
-/**
- * @author Dvir
- *
+ * @author Dvir Azulay, Or Elmaliach
+ * This is a simple interface describing the mandatory actions of any queue-based ADT, which is working with Page
  */
 public interface Queue {
 	public void enqueue(Page page);
 	public Page dequeue();
+	public boolean isEmpty();
 }
