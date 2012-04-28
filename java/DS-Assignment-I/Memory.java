9d4e3c962f285858cc0fec1b59f6b93154a221b3
orelma <orelma@post.bgu.ac.il>
2012-04-28 18:16:22 +0300

    Update java/DS-Assignment-I/Memory.java

--- java/DS-Assignment-I/Memory.java
+++ java/DS-Assignment-I/Memory.java
@@ -1,10 +1,7 @@
-/**
- * 
- */
 
 /**
- * @author Dvir
- *
+ * @author Dvir Azulay, Or Elmaliach
+ * This is a simple interface to describe the mandatory actions that are required from any memory-type class
  */
 public interface Memory {
 	public String read(int key);
