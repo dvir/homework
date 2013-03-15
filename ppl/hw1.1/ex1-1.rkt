cfe63fcb0b82a2e9841049c511942e7de1f17520
Dvir Azulay <dvir.azulay@gmail.com>
2013-03-15 17:43:16 +0200

    finished questions 1-3 in hw1.1

--- ppl/hw1.1/ex1-1.rkt
+++ ppl/hw1.1/ex1-1.rkt
@@ -1,27 +1,71 @@
-#lang scheme
+;--------------;
+;  question 3  ;
+;--------------;
 ; Signature: deep-sum-digit(n)
 ; Type: [Number -> Number]
 ; Purpose: compute the (deep) sum of digits of a number.
-; Pre-conditions: n >= 0
+; Pre-conditions: n >= 0, integer
 ; Tests: (deep-sum-digit 4526) => 8
 (define deep-sum-digit (lambda(y)
                          (
                           if (< y 10)
-                             (display y)
+                             y
                              (deep-sum-digit (sum-digit y))
                          )
                        )
 )
 
-(define sum-digit-internal (lambda(y sum)
+; Signature: sum-digit(n)
+; Type: [Number -> Number]
+; Purpose: compute (simple) sum of digit of a number.
+; Pre-conditions: n >= 0, integer
+; Tests: (sum-digit 12) => 3
+(define sum-digit (lambda(y)
               (
                if (< y 10) 
-                  (+ y sum) 
-                  (sum-digit-internal 
-                      (floor (/ y 10)) 
-                      (+ sum (modulo y 10))
-                   )
+                  y 
+                  (+
+                     (sum-digit (floor (/ y 10))) 
+                     (modulo y 10)
+                  )
               )
             )
 )
-(define sum-digit (lambda(y) (sum-digit-internal y 0)))
+
+
+
+;--------------;
+;  question 4  ;
+;--------------;
+; Signature: calc-1-e(n)
+; Type: [Number -> Number]
+; Purpose: Approximates 1/e as the sum of n first elements from n_i=(-1)^i/i!
+; Pre-conditions: n >= 0, integer 
+; Tests: 
+(define calc-1-e (lambda(n) (
+                    if (= n 0)
+                       1
+                       (+
+                          (calc-1-e (- n 1))
+                          (/
+                             (if (= 0 (modulo n 2))
+                                 1
+                                 -1
+                             )
+                             (factorial n)
+                          )
+                       )
+                 ) 
+))
+
+; Signature: factorial(n)
+; Type: [Number -> Number]
+; Purpose: Computer the factorial (!) of a number n.
+; Pre-conditions: n >= 0, integer
+; Tests: (factorial 5) => 120
+(define factorial (lambda(n) (
+                     if (= n 0)
+                        1
+                        (* n (factorial (- n 1)))
+                  )
+))
