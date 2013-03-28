47215c28cb4eb8e5e5f8a0d55fde02d8e30767b5
Dvir Azulay <dvir.azulay@gmail.com>
2013-03-28 14:14:58 +0200

    fixed according to assignment changes. Added contracts

--- ppl/hw1.2/ex1-2.rkt
+++ ppl/hw1.2/ex1-2.rkt
@@ -1,13 +1,13 @@
 ;----------------;
 ;  Question 1.a  ;
 ;----------------;
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: ln2-rec(n)
+; Type: [Number -> Number]
+; Purpose: Recursively compute the sum of the first n elements in the ln2 series
+; Example: (ln2-rec 4)
+; Pre-conditions: n >= 0
+; Post-conditions: return sum of first n elements in ln2 series
+; Tests: (ln2-rec 2) => 1/2
 (define ln2-rec 
   (lambda(n)
     (if (= n 0)
@@ -26,13 +26,13 @@
 ;----------------;
 ;  Question 1.b  ;
 ;----------------;
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: third-iter(n, acc)
+; Type: Number*Number -> Number
+; Purpose: Iteratively compute the sum of the first n elements in the third series (+ acc)
+; Example: (third-iter 4 0)
+; Pre-conditions: n >= 0
+; Post-conditions: return (acc + sum of first n elements in third series)
+; Tests: (third-iter 3 0) => 3/8
 (define third-iter
   (lambda(n acc)
     (if (= n 0)
@@ -57,24 +57,21 @@
 ;----------------;
 ;  Question 1.c  ;
 ;----------------;
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
+; Signature: sum-alt(term, a, next, b)
+; Type: [Number -> Number] * Number * [Number -> Number] * Number -> Number
+; Purpose: Compute the sum of the alternating series in the range [a, b], starting from a, jumping with (next a) and adding (term a) to the sum.
+; Example: (sum-alt (lambda(x)x) 1 (lambda(x)(+ 1 x)) 3)
 ; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Post-conditions: return the sum of (term a) - (term (next a)) + (term (next (next a))) ... +- (term (next .. (next a) .. ))
+; Tests: (sum-alt (lambda(x)x) 1 (lambda(x)(+ 1 x)) 4) => -2
 (define sum-alt
   (lambda(term a next b)
     (if (> a b)
        0
        (+
-          (*
-             (if (= (modulo a 2) 0) -1 1 )
-             (term a)
-          )
+          (term a)
           (sum-alt 
-             term
+             (lambda(x) (* -1 (term x)))
              (next a) 
              next 
              b
@@ -87,39 +84,39 @@
 ;----------------;
 ;  Question 1.d  ;
 ;----------------;
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: term-ln2(n)
+; Type: [Number -> Number]
+; Purpose: Compute the term corresponding to n in the ln2 series
+; Example: (term-ln2 2)
+; Pre-conditions: n != 0
+; Post-conditions: return 1/n
+; Tests: (term-ln2 2) => 1/2
 (define term-ln2
   (lambda(n)
     (/ 1 n)
   )
 )
 
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
+; Signature: next-ln2(n)
+; Type: [Number -> Number]
+; Purpose: Compute the next number in the ln2 series to be passed to term-ln2
+; Example: (next-ln2 2)
 ; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Post-conditions: return n+1
+; Tests: (next-ln2 3) => 4
 (define next-ln2
   (lambda(n)
     (+ n 1)
   )
 )
 
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: term-third(n)
+; Type: [Number -> Number]
+; Purpose: Compute the term corresponding to n in the third series
+; Example: (term-third 2)
+; Pre-conditions: n >= 0
+; Post-conditions: return 1/(2^n)
+; Tests: (term-third 3) => 1/8
 (define term-third
   (lambda(n)
     (/ 
@@ -129,13 +126,13 @@
   )
 )
 
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: next-third(n)
+; Type: [Number -> Number]
+; Purpose: Compute the next number in the third series to be passed to term-third
+; Example: (next-third 2)
+; Pre-conditions: 
+; Post-conditions: return n+1
+; Tests: (next-third 3) => 4
 (define next-third
   (lambda(n)
     (+ n 1)
@@ -145,13 +142,13 @@
 ;----------------;
 ;   Question 2   ;
 ;----------------;
-; Signature:
-; Type: 
-; Purpose: 
-; Example: 
-; Pre-conditions:
-; Post-conditions:
-; Tests: 
+; Signature: con-func(f, a, flag)
+; Type: [Number -> Number] * Number * (Number Union Type) -> [Number -> Number]
+; Purpose: Produce g(x) according to f(x), number a and a flag. flag == 0 - g(x) = f(x + a), flag == 1 - g(x) = f(x - a), flag == 2 - g(x) = f(x * a), flag == 3 - g(x) = f(x / a), else - g(x) = f(x).
+; Example: (con-func (lambda(x) (x)) 2 0)
+; Pre-conditions: (flag != 3) OR (flag == 3 AND a != 0)
+; Post-conditions: 
+; Tests: ((con-func (lambda(x) (x)) 2 0) 3) => 5
 (define con-func
   (lambda(f a flag)
       (cond
