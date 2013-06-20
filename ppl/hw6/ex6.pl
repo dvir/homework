a651cd6ad43704ffda1a8e12d1c5d581fa54e51b
Dvir Azulay <dvir.azulay@gmail.com>
2013-06-20 13:49:29 +0300

    Finish PPL hw6 coding part

--- ppl/hw6/ex6.pl
+++ ppl/hw6/ex6.pl
@@ -1,48 +1,3 @@
-% A relational database for supermarket grocery management.
-% The database contains the tables: product, product_category and vendor_info.
-
-% Signature: product(Barcode, ProductName, Category, Refrigerated, VendorName)/5
-% Purpose: details on available products in the supermarket.
-%
-product(972000232542, fresh_milk,     01,  true, dairy_inc).
-product(972000237599, low_fat_milk,   01,  true, more_milk).
-product(972000232543, preserved_milk, 01, false, dairy_inc).
-product(972000756575, white_bread,    02, false, angy).
-product(972000756576, rye_bread,      02, false, angy).
-product(972000756577, pita,           02, false, breadman).
-product(972000556756, chips,          03, false, much_salt).
-product(972000767847, chips,          03, false, tasty_products).
-product(972000767848, crisps,         03, false, tasty_products).
-product(972000684856, cola,           04,  true, cool_drinks).
-product(972000456745, lighter,        05, false, fire_ltd).
-
-% Signature: product_category(Id, CategoryName)/2
-% Purpose: specify product categories.
-%
-product_category(01, dairy).
-product_category(02, bread).
-product_category(03, snack).
-product_category(04, soft_drinks).
-product_category(05, misc).
-
-% Signature: vendor_info(VendorName, Phone, MonthlyContactDay)/3
-% Purpose: specify vendor contact information.
-%
-vendor_info(dairy_inc,      4564564, 5).
-vendor_info(more_milk,      4564567, 15).
-vendor_info(angy,           4574746, 28).
-vendor_info(breadman,       8877665, 10).
-vendor_info(much_salt,      5456464, 28).
-vendor_info(tasty_products, 2002001, 28).
-vendor_info(cool_drinks,    6785785, 15).
-vendor_info(fire_ltd,       7855775, 10).
-
-% Signature: not_member(Element, List)/2
-% Purpose: the relation in which Element is not a member of a List.
-%
-not_member(_, []).
-not_member(X, [Y|Ys]) :- X \= Y, 
-                         not_member(X, Ys).
 % Question 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 1.1
 % Signature: check_barcodes_product(Barcode)/1
@@ -141,12 +96,12 @@ unrefrigerated_ordering(CategoryName, MonthlyContactDay) :-
 % CategoryDayPairsList = [(misc, 10), (snack, 28), (bread, 10), (bread, 28), (dairy, 5)]
 %
 
-unrefrigerated_ordering_list([]).
-unrefrigerated_ordering_list([H|T]) :- 
-    H = (CategoryName, MonthlyContactDay),
-    unrefrigerated_ordering(CategoryName, MonthlyContactDay),
-    not_member((CategoryName, MonthlyContactDay), T),
-    unrefrigerated_ordering_list(T).
+%unrefrigerated_ordering_list([]).
+%unrefrigerated_ordering_list([H|T]) :- 
+    %H = (CategoryName, MonthlyContactDay),
+    %unrefrigerated_ordering(CategoryName, MonthlyContactDay),
+    %not_member((CategoryName, MonthlyContactDay), T),
+    %unrefrigerated_ordering_list(T).
 
 % Question 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2.1
@@ -167,10 +122,14 @@ unrefrigerated_ordering_list([H|T]) :-
 % false
 %
 
-deep_reverse([], []).
+deep_reverse([], []) :- !.
 deep_reverse([H|T], List) :-
-    append(X, [H], List),
-    deep_reverse(T, X).
+    !,
+    deep_reverse(T, RT),
+    deep_reverse(H, RH),
+    append(RT, [RH], List).
+
+deep_reverse(X, X).
 
 % 2.2
 % Signature: sublist_perm(Full, PermSub)/2
@@ -211,6 +170,26 @@ deep_reverse([H|T], List) :-
 % X = [c, a, b] ;
 % X = [c, b, a]
 %
+
+% Signature: delete(Needle, Haystack, Result)/3
+% Purpose: Delete the first occurrence of Needle in Haystack.
+delete(Needle, [], []).
+delete(Needle, Haystack, Result) :-
+    delete_help(Needle, Before, Haystack, Result).
+
+delete_help(Needle, Before, [Needle|After], Result) :-
+    append(Before, After, Result),!.
+
+delete_help(Needle, Before, [X|After], Result) :-
+    append(Before, [X], Z),
+    delete_help(Needle, Z, After, Result),!.
+
+sublist_perm(X, []).
+sublist_perm([], X) :- !, false.
+sublist_perm(Full, [H|T]) :-
+    member(H, Full),
+    delete(H, Full, Without),
+    sublist_perm(Without, T).
 
 % 2.3
 % Signature: s(Z)/1
@@ -280,6 +259,9 @@ p([Z])   :- member(Z, [in, from, on]).
 % false
 %
 
+adv([Z]) :- Z=[] ; (member(Z, [hungrily, slowly, gladly])).
+vp(Z)    :- append(F, T, Z), append(A, Y, F), append(V, X, A), v(V), np(X), pp(Y), adv(T).
+
 % 2.3.b
 % Signature: subCFG(Text, SubText)/2
 % Purpose: SubText is sublist of a permutation of Text, 
@@ -305,3 +287,8 @@ p([Z])   :- member(Z, [in, from, on]).
 %           [a, tall, cat, ate, a, big, mouse, on, a, funny, dog]).
 % false
 %
+
+subCFG(Text, SubText) :- 
+    s(Text), 
+    s(SubText),
+    sublist_perm(Text, SubText).
